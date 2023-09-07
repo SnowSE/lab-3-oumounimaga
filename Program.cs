ba6808e26d54a5374a8f814947e12f177c9364b7
Oumou Nimaga <oumou.nimaga@students.snow.edu>
2023-09-07 14:24:17 -0600

    done

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("    A     ");
+Console.WriteLine("   BCD   ");
+Console.WriteLine("  EFDHI ");
+Console.WriteLine(" JKLMNOP ");
+Console.WriteLine(" QRSTUVWXYZ");
+Console.WriteLine("    A      \n   BCD   \n  EFDHI  \n JKLMNOP \nQRSTUVWXYZ");
+Console.WriteLine("\n");
+
+Console.Write("enter first number: ");
+int x = int.Parse(Console.ReadLine());
+
+Console.Write("enter second number: ");
+int y = int.Parse(Console.ReadLine());
+Console.WriteLine("\n");
+Console.WriteLine("Sum is............" + (x + y));
+Console.WriteLine("Difference is...." + (x-y));
+Console.WriteLine("Product is........" + (x*y));
+Console.WriteLine("Quotient is......." + (x/y));
